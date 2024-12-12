fc1682d53f84a578ebf2e1e439638d3a3a042553
SavadorCardona <hebustha@gmail.com>
2024-12-12 16:36:31 +0100

    Fix Make File

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,11 @@ import ollama from "ollama";
 
 export default function App() {
   const [data, setData] = useState();
-  const [text, setText] = useState();
+  const [text, setText] = useState<string>("");
 
   async function send() {
     console.log("rentre");
+
     const resp = await ollama.list();
     console.log(resp);
     const response = ollama.chat({
@@ -17,16 +18,22 @@ export default function App() {
       messages: [{ role: "user", content: text }]
     }).then(response => {
       console.log(response);
+      setText("");
       setData(response.message.content);
     });
+  }
+
+  function pressEnter(code: string) {
+    if (code === "Enter") send();
   }
 
   return (
     <Theme>
       <div
-        className="flex flex-col min-h-screen  text-center bg-white dark:bg-gray-700 flex align-center center w-[900px] p-5">
+        className="flex flex-col content-center  min-h-screen  text-center bg-white dark:bg-gray-700 w-[900px] p-5">
         <Box style={{ background: "var(--gray-a2)", borderRadius: "var(--radius-3)" }} width={"500px"}>
-          <TextArea size="1" placeholder="Reply to comment…" onChange={(e) => setText(e.target.value)} />
+          <TextArea size="1" value={text} placeholder="Reply to comment…" onChange={(e) => setText(e.target.value)}
+                    onKeyDown={(e) => pressEnter(e.code)} />
           <Button onClick={send}>
             Run
           </Button>
